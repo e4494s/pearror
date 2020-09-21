c709a31e016959c52db42ac4158cf831131033b2
e4494s <61923287+e4494s@users.noreply.github.com>
2020-09-21 14:12:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,10 @@ client.on('message', message => {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
 	console.log(message.content);
-	if (message.content === '~ping') {
+	if (message.content == '~help') {
+		message.channel.send('``ping`: Return bot latency\n`~echo`: Copy what you say');
+	}
+	else if (message.content === '~ping') {
 		message.channel.send(`Pong, ${message.member.user.username}! \`${Date.now() - message.createdTimestamp}ms\``);
 	}
   	else if (message.content.startsWith('~echo')) {
