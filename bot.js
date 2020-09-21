71ac17895ae68c49820c2f984ef6ef2878664ef6
e4494s <61923287+e4494s@users.noreply.github.com>
2020-09-21 14:43:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,19 +10,31 @@ client.once('ready', () => {
 client.on('message', message => {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
-	console.log(message.content);
-	if (message.content == '~help') {
+	let cmd = message.content.split(' ');
+	*if (cmd[0] == '~help') {
+		message.channel.send('`~ping`: Return bot latency\n`~echo`: Copy what you say\n`~dice `__`n`__: Roll an __n__ sided dice');
+	}
+	else if (cmd[0] === '~ping') {
+		message.channel.send(`Pong, ${message.member.user.username}! \`${Date.now() - message.createdTimestamp}ms\``);
+	}
+  	else if (cmd[0] == '~echo') {
+    		message.channel.send(message.content.replace('~echo', ''));
+    	}
+	else if (cmd[0] == '~dice') {
+		message.channel.send(Math.ceil(Math.random() * Number(message.content.replace('~dice ', ''))));
+	}
+	/*if (message.content == '~help') {
 		message.channel.send('`~ping`: Return bot latency\n`~echo`: Copy what you say\n`~dice `__`n`__: Roll an __n__ sided dice');
 	}
 	else if (message.content === '~ping') {
 		message.channel.send(`Pong, ${message.member.user.username}! \`${Date.now() - message.createdTimestamp}ms\``);
 	}
   	else if (message.content.startsWith('~echo')) {
-    	message.channel.send(message.content.replace('~echo', ''));
-    }
+    		message.channel.send(message.content.replace('~echo', ''));
+    	}
 	else if (message.content.startsWith('~dice')) {
 		message.channel.send(Math.ceil(Math.random() * Number(message.content.replace('~dice ', ''))));
-	}
+	}*/
 });
 
 client.login(process.env.BOT_TOKEN);
