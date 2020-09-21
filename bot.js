9696026b4d4f8307e5636d98aa11a62df4fbd86f
e4494s <61923287+e4494s@users.noreply.github.com>
2020-09-21 09:42:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,10 +12,7 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix)) return;
 	console.log(message.content);
 	if (message.content === '~ping') {
-                let user = message.member;
-                user = user.toString();
-                user = client.users.get(user).username;
-		message.channel.send(`Pong, ${message.member}!`);
+		message.channel.send(`Pong, ${message.member.toString()}!`);
 	}
 });
 
