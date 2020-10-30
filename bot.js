3576e1eac30797669c59924fc3516941d9b7ccaf
e4494s <61923287+e4494s@users.noreply.github.com>
2020-10-30 10:10:15 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -175,7 +175,7 @@ function titleCase(string) {
 //
 
 client.once('ready', () => {
-        client.channels.get('754200846530641960').send('`Changes successfully loaded.` *I am alive...*');
+        //client.channels.get('754200846530641960').send('`Changes successfully loaded.` *I am alive...*');
 });
 
 client.on('message', message => {
