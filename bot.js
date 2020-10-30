a950b082493b7d907bbc1286b36b6e726204b656
e4494s <61923287+e4494s@users.noreply.github.com>
2020-10-29 21:47:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,7 +96,7 @@ function makeSentence() {
 //
 
 client.once('ready', () => {
-        client.channels.get('754200846530641960').send('`Changes successfully loaded.` *I am alive...*');
+        //client.channels.get('754200846530641960').send('`Changes successfully loaded.` *I am alive...*');
 });
 
 client.on('message', message => {
