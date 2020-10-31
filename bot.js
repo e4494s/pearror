b9e7358a7b130db06155d554c97988bcde604391
e4494s <61923287+e4494s@users.noreply.github.com>
2020-10-31 14:51:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -260,8 +260,8 @@ client.once('ready', () => {
 client.on('message', message => {
         if (message.content.toLowerCase().includes('pear') || message.content.toLowerCase().includes('🍐')) message.react('🍐');
 	if (message.content.includes('~poll')) {
-		message.react('👍');
-		message.react('👎');
+		await message.react('👍');
+		await message.react('👎');
 	}
         if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
