d072504de1c2d6c943c5afd07cd8e9414fd53119
e4494s <61923287+e4494s@users.noreply.github.com>
2020-11-22 20:24:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -324,14 +324,11 @@ client.on('message', message => {
 		message.channel.send(`Pong, ${message.author.username}! \`${Date.now() - message.createdTimestamp}ms\``);
 	}
   	else if (cmd[0] == '~echo') {
-		try{
 		let mbd = {
 			color: 0xffffff,
 			description: message.content.replace('~echo', '')
 		};
-    		message.channel.send({embed: mdb});
-		}
-		catch(er){message.channel.send(er.message);}
+    		message.channel.send({embed: mbd});
     	}
 	else if (cmd[0] == '~dice') {
 		if (cmd.length == 1 || typeof(Number(cmd[1])) !== 'number') { message.channel.send('Please input a number of sides.'); return; }
